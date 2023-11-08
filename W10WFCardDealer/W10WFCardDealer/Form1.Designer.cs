42db6b0f22235be39fd3aafc8f8e9be763f4d0d7
jungbk0808 <jungbk0808@gmail.com>
2023-11-08 15:12:10 +0900

    add random deal

--- W10WFCardDealer/W10WFCardDealer/Form1.Designer.cs
+++ W10WFCardDealer/W10WFCardDealer/Form1.Designer.cs
@@ -30,7 +30,15 @@
         {
             card1 = new PictureBox();
             btnDeal = new Button();
+            card2 = new PictureBox();
+            card3 = new PictureBox();
+            card4 = new PictureBox();
+            card5 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)card1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)card2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)card3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)card4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)card5).BeginInit();
             SuspendLayout();
             // 
             // card1
@@ -38,31 +46,79 @@
             card1.Image = Properties.Resources._10_of_hearts;
             card1.Location = new Point(12, 12);
             card1.Name = "card1";
-            card1.Size = new Size(264, 386);
+            card1.Size = new Size(170, 247);
             card1.SizeMode = PictureBoxSizeMode.Zoom;
             card1.TabIndex = 0;
             card1.TabStop = false;
             // 
             // btnDeal
             // 
-            btnDeal.Location = new Point(12, 406);
+            btnDeal.Location = new Point(364, 265);
             btnDeal.Name = "btnDeal";
-            btnDeal.Size = new Size(264, 58);
+            btnDeal.Size = new Size(170, 247);
             btnDeal.TabIndex = 1;
             btnDeal.Text = "카드 분배";
             btnDeal.UseVisualStyleBackColor = true;
             btnDeal.Click += OnDeal;
             // 
+            // card2
+            // 
+            card2.Image = Properties.Resources._10_of_hearts;
+            card2.Location = new Point(188, 12);
+            card2.Name = "card2";
+            card2.Size = new Size(170, 247);
+            card2.SizeMode = PictureBoxSizeMode.Zoom;
+            card2.TabIndex = 0;
+            card2.TabStop = false;
+            // 
+            // card3
+            // 
+            card3.Image = Properties.Resources._10_of_hearts;
+            card3.Location = new Point(364, 12);
+            card3.Name = "card3";
+            card3.Size = new Size(170, 247);
+            card3.SizeMode = PictureBoxSizeMode.Zoom;
+            card3.TabIndex = 0;
+            card3.TabStop = false;
+            // 
+            // card4
+            // 
+            card4.Image = Properties.Resources._10_of_hearts;
+            card4.Location = new Point(12, 265);
+            card4.Name = "card4";
+            card4.Size = new Size(170, 247);
+            card4.SizeMode = PictureBoxSizeMode.Zoom;
+            card4.TabIndex = 0;
+            card4.TabStop = false;
+            // 
+            // card5
+            // 
+            card5.Image = Properties.Resources._10_of_hearts;
+            card5.Location = new Point(188, 265);
+            card5.Name = "card5";
+            card5.Size = new Size(170, 247);
+            card5.SizeMode = PictureBoxSizeMode.Zoom;
+            card5.TabIndex = 0;
+            card5.TabStop = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(288, 476);
+            ClientSize = new Size(545, 521);
             Controls.Add(btnDeal);
+            Controls.Add(card3);
+            Controls.Add(card5);
+            Controls.Add(card2);
+            Controls.Add(card4);
             Controls.Add(card1);
             Name = "Form1";
             Text = "카드 딜러";
             ((System.ComponentModel.ISupportInitialize)card1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)card2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)card3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)card4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)card5).EndInit();
             ResumeLayout(false);
         }
 
@@ -70,5 +126,9 @@
 
         private PictureBox card1;
         private Button btnDeal;
+        private PictureBox card2;
+        private PictureBox card3;
+        private PictureBox card4;
+        private PictureBox card5;
     }
 }
--- W10WFCardDealer/W10WFCardDealer/Form1.cs
+++ W10WFCardDealer/W10WFCardDealer/Form1.cs
@@ -17,9 +17,68 @@ namespace W10WFCardDealer
             //? 대신 var를 데이터 타입으로 쓰면 자동 nullable
             //Bitmap? b = Properties.Resources.ResourceManager.GetObject("10_of_diamonds") as Bitmap;
 
-            Image? b = Properties.Resources.ResourceManager.GetObject("jack_of_clubs2") as Image;
+            //Image? b = Properties.Resources.ResourceManager.GetObject("jack_of_clubs2") as Image;
 
-            card1.Image = b;
+            Random r = new Random();
+            //int n = r.Next(52);
+            int[] nums = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                nums[i] = r.Next(52);
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] == nums[i])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            //Image? b = Properties.Resources.ResourceManager.GetObject(GetCardName(n)) as Image;
+            Image?[] b = new Image?[5];
+            for (int i = 0; i < 5; i++)
+            {
+                b[i] = Properties.Resources.ResourceManager.GetObject(GetCardName(nums[i])) as Image;
+            }
+            card1.Image = b[0];
+            card2.Image = b[1];
+            card3.Image = b[2];
+            card4.Image = b[3];
+            card5.Image = b[4];
+        }
+        protected string GetCardName(int n)
+        {
+            //TODO: 하드 코딩 변경할 것
+            string suit = string.Empty; //""의 의미
+            switch (n / 13)
+            {
+                case 0: suit = "spades"; break;
+                case 1: suit = "diamonds"; break;
+                case 2: suit = "hearts"; break;
+                case 3: suit = "clubs"; break;
+                default: suit = "error"; break;
+            }
+            string rank = string.Empty;
+            switch (n % 13)
+            {
+                case 0: rank = "ace"; break;
+                case int c when (c > 0 && c < 10):
+                    rank = (n % 13 + 1).ToString();
+                    break;
+                case 10: rank = "jack"; break;
+                case 11: rank = "queen"; break;
+                case 12: rank = "king"; break;
+                default: rank = "error"; break;
+            }
+            if (n % 13 >= 10 && n % 13 <= 12)
+            {
+                suit += "2"; //jack, queen, king이면 2를 붙여야 함
+            }
+
+            //return rank + "_of_" + suit;
+            //return string.Format("{0}_of_{1}", rank, suit);
+            return $"{rank}_of_{suit}";
         }
     }
 }
