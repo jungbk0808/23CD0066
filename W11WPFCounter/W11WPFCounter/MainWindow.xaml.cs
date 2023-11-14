dcda870a8c7920af62fae38f9a3046b20e4a8b98
jungbk0808 <jungbk0808@gmail.com>
2023-11-14 16:32:40 +0900

    231114_01

---
+++ W11WPFCounter/W11WPFCounter/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W11WPFCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected int Count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TxtCount.Text = Count.ToString();
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            TxtCount.Text = $"{++Count}";
+        }
+
+        private void OnSub(object sender, RoutedEventArgs e)
+        {
+            if (Count > 0)
+            {
+                TxtCount.Text = $"{--Count}";
+            }
+        }
+    }
+}
