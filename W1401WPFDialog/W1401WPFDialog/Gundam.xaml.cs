ef17cc318f8fb10399759f39b64835fb56c9fa23
jungbk0808 <jungbk0808@gmail.com>
2023-12-05 16:18:38 +0900

    🚀 다이얼로그 완성

---
+++ W1401WPFDialog/W1401WPFDialog/Gundam.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+
+namespace W1401WPFDialog;
+
+public partial class Gundam : Window
+{
+    public string MSName { get { return Name2.Text; } }
+    public string MSModel { get { return Model.Text; } }
+    public string MSParty => Party.Text;
+
+
+    public Gundam()
+    {
+        InitializeComponent();
+        Name2.Focus();
+    }
+
+    public void OnOk(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Name2.Text))
+        {
+            MessageBox.Show(
+                "이름을 입력하십시오.",
+                "입력 부족",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+            return;
+        }
+        if (string.IsNullOrEmpty(Model.Text))
+        {
+            MessageBox.Show(
+                "모델을 입력하십시오.",
+                "입력 부족",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+            return;
+        }
+        if (string.IsNullOrEmpty(Party.Text))
+        {
+            MessageBox.Show(
+                "소속을 입력하십시오.",
+                "입력 부족",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+            return;
+        }
+        DialogResult = true;
+    }
+
+    public void OnCancel(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+    }
+}
---
+++ W1401WPFDialog/W1401WPFDialog/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace W1401WPFDialog;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void OnAdd(object sender, RoutedEventArgs e)
+    {
+        Gundam g = new Gundam();
+        if (g.ShowDialog() != true)
+            return;
+
+        Result.Text = $"{g.MSParty}의 {g.MSModel} "
+            + $"{g.MSName}{(HasJongsung(g.MSName) ? "이" : "가")} "
+            + "추가되었습니다.\n"
+            + Result.Text;
+    }
+
+    public bool HasJongsung(string str)
+    {
+        if (str.Length < 1)
+            return true;
+        char last = str[str.Length - 1];
+        return (last - 44032) % 28 != 0 ? true : false;
+    }
+}
